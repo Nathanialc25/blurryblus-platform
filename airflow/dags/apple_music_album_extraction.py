9391bf60298090c56f1e6918eab711c93e6a70e4
Nate Carter <nathanialc17@gmail.com>
2025-11-24 17:57:17 -0700

    adjusting the view sql

--- airflow/dags/apple_music_album_extraction.py
+++ airflow/dags/apple_music_album_extraction.py
@@ -83,7 +83,9 @@ ON CONFLICT (url) DO NOTHING;
 """
 
 VIEW_SQL = f"""
-CREATE OR REPLACE VIEW {VIEW_NAME} AS
+DROP VIEW IF EXISTS v_weekly_new_releases;
+
+CREATE VIEW {VIEW_NAME} AS
 SELECT 
 
     complex_id, 
@@ -91,15 +93,13 @@ SELECT
     artist, 
     release_date, 
     track_count, 
-    array_to_string(array_remove(string_to_array(genre, ','), ' Music'), ',') as genre, #removing the Music from each of those arrays, probably should be done else where
+    array_to_string(array_remove(string_to_array(genre, ','), ' Music'), ',') as genre,
     url, 
     editorial_notes, 
     cover_art_url
 FROM {SCHEMA}.{TABLE_NAME}
 WHERE 
     DATE(release_date) BETWEEN (CURRENT_DATE - INTERVAL '6 days') AND CURRENT_DATE
-    AND 
-    DATE(load_date) BETWEEN (CURRENT_DATE - INTERVAL '6 days') AND CURRENT_DATE;
 """
 
 def generate_album_id(album_data: dict) -> str:
